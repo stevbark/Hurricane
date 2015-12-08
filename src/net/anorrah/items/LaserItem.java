cf6366479b7859c9e6bc501ebdfba3f6ee545629
richy teas <richyteas@gmail.com>
2015-12-07 17:10:14 -0800

    Menu (ESC) work in progress

--- src/net/anorrah/items/LaserItem.java
+++ src/net/anorrah/items/LaserItem.java
@@ -11,12 +11,10 @@ public class LaserItem extends RangedWeaponItem{
 		hasCharges = true;
 		charges = 7;
 		damage = (1*enchantment)+10;
-<<<<<<< HEAD
+
 	//	myBonus.add(new ());
 		name="Laser";
-=======
 		myBonus.add(new laserBonus((int) damage));
->>>>>>> c7b58b8bbf2e21bde3279f66095861884c4c5644
 		
 		// TODO Auto-generated constructor stub
 	}
